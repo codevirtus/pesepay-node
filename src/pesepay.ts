a07a666c18ab4b58f5790f9576abe8da3c41df75
viper75 <sean.huvaya03@yahoo.com>
2021-10-14 12:47:10 +0200

    Update README.md file

--- src/pesepay.ts
+++ src/pesepay.ts
@@ -3,9 +3,9 @@ import { InvalidRequestError } from "./exceptions/invalid-request-error";
 import { PesepaySeamlessTransaction } from "./payments/pesepay-seamless-transaction";
 import axios from 'axios';
 import { PaymentResponse } from "./payments/payment-response";
-import { CreateTransactionCommand } from "./payments/create-transaction-command";
 import { AmountDetails } from "./payments/amount-details";
 import { PaymentProcessingContext } from "./payments/payment-processing-context";
+import { CreateTransactionCommand } from "./payments/create-transaction";
 
 
 const BASE_URL = 'https://api.test.pesepay.com/api/payments-engine';
@@ -24,18 +24,17 @@ export class Pesepay {
         this.headers = { 'key': this.integrationKey }
     }
 
-    initiateTransaction = async(applicationId: number, applicationName: string, applicationCode: string, reason: string, amount: number, currencyCode: string, reference?: string): Promise<any> => {
+    initiateTransaction = async(transaction: CreateTransactionCommand): Promise<any> => {
         if (this.resultUrl == null)
             throw new InvalidRequestError('Result url has not beeen specified.');
 
         if (this.returnUrl == null)
             throw new InvalidRequestError('Return url has not been specified.');
 
-        let amountDetails = new AmountDetails(amount, currencyCode);
-        let rawRequest = new CreateTransactionCommand(applicationId, applicationName, applicationCode, reason, this.resultUrl, this.returnUrl, amountDetails);
-        rawRequest.merchantReference = reference;
+        transaction.resultUrl = this.resultUrl;
+        transaction.returnUrl = this.returnUrl;
 
-        let ecnryptioncontext = new EncryptionContext(JSON.stringify(rawRequest), this.encryptionKey);
+        let ecnryptioncontext = new EncryptionContext(JSON.stringify(transaction), this.encryptionKey);
 
         let payload = Cryptography.encrypt(ecnryptioncontext);
 
@@ -103,7 +102,7 @@ export class Pesepay {
         try {
             let response = await axios.get(`${BASE_URL}/v1/payments/check-payment?referenceNumber=${referenceNumber}`, { headers: this.headers });
             let decryptContext = new EncryptionContext(response.data['payload'], this.encryptionKey);
-            return JSON.parse(Cryptography.decrypt(decryptContext))['transactionStatus']
+            return JSON.parse(Cryptography.decrypt(decryptContext));
         } catch (error: any) {
             throw new Error(error.response.data.message || 'Something went wrong!');
         }
